a68f5b45c65ee970afa88fe8f4c63e6428b45bca
Maryan <83982869+mah752@users.noreply.github.com>
2023-06-26 18:54:40 +0100

    added files

---
+++ script.js
@@ -0,0 +1,18 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+const closeIcon = document.querySelector('.close-icon');
+
+burger.addEventListener('click', () => {
+  menu.classList.toggle('show');
+});
+
+closeIcon.addEventListener('click', () => {
+  menu.classList.remove('show');
+});
+
+document.addEventListener('click', (event) => {
+  const target = event.target;
+  if (!target.closest('.menu') && !target.closest('.burger')) {
+    menu.classList.remove('show');
+  }
+});
